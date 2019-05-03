230c0605c0007e29ab917d1620915cce3655cd3d
Codrin Iftimie <iceboxic@gmail.com>
2019-05-03 21:39:52 +0300

    Move article example

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import React from 'react';
-import Article, {AlternativeArticle} from './Article';
-import {render} from 'react-dom';
-import ArticleMainLayout, {ArticleAlternativeLayout} from './ArticleLayouts';
-import './base.css';
+import { render } from "react-dom";
+import ArticleExample from './examples/Article';
 
-const article={
-  title: "Layouts are awesome",
-  author: "Codrin Iftimie",
-  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
-
-const app = (
-  <>
-    <ArticleMainLayout>
-      <Article data={article}/>
-    </ArticleMainLayout>
-    <ArticleMainLayout>
-      <AlternativeArticle data={article}/>
-    </ArticleMainLayout>
-    <ArticleAlternativeLayout>
-      <Article data={article}/>
-    </ArticleAlternativeLayout>
-    <ArticleAlternativeLayout>
-      <AlternativeArticle data={article}/>
-    </ArticleAlternativeLayout>
-  </>
-)
-
-render(app, document.getElementById('root'));
+render(<ArticleExample />, document.getElementById('root'));
